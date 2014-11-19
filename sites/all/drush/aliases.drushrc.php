1c9621ad54f507d3897255d15674b87ad3252f7b
Chris Morris <cmorris@ccistudios.com>
2014-11-19 10:18:05 -0500

    Added live drush alias

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -11,3 +11,14 @@ $aliases['dev'] = array(
 	'ssh-options'=>'-p 37241'
 );
 
+$aliases['live'] = array(
+	'uri'=> 'live.trusses.ca',
+	'root' => '/home/trusses/subdomains/live/public_html',
+	'remote-host'=> 'host.ccistudios.com',
+	'remote-user'=> 'trusses',
+	'path-aliases'=> array(
+		'%files'=> 'sites/default/files',
+	),
+	'ssh-options'=>'-p 37241'
+);
+
