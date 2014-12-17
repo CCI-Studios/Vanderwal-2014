5e0aa059dbaa506a15091f697b18ba9e76cab608
Chris Morris <cmorris@ccistudios.com>
2014-12-17 09:58:12 -0500

    Change the FAQ image on products/services

--- sites/all/themes/vanderwal/js/sliders.js
+++ sites/all/themes/vanderwal/js/sliders.js
@@ -3,7 +3,7 @@
         boxes().click(clickBox);
         $(".views-field-view-node").click(clickClose);
 		
-		$(".view--products-services-boxes .view-content").append('<div class="views-row"><div class="views-field views-field-title"><span class="field-content"><a href="/faqs" style="pointer-events:all;">FAQs</a></span></div><div class="views-field views-field-field-upload-image"><div class="field-content"><img typeof="foaf:Image" src="/sites/default/files/styles/steel_advantage_thumbnail/public/s_img4_1.jpg" width="231" height="231" alt=""></div></div><div class="views-field views-field-view-node"><span class="field-content"><a href="/faqs">+</a></span></div></div>');
+		$(".view--products-services-boxes .view-content").append('<div class="views-row"><div class="views-field views-field-title"><span class="field-content"><a href="/faqs" style="pointer-events:all;">FAQs</a></span></div><div class="views-field views-field-field-upload-image"><div class="field-content"><img typeof="foaf:Image" src="/sites/default/files/styles/steel_advantage_thumbnail/public/s_img_1.jpg" width="231" height="231" alt=""></div></div><div class="views-field views-field-view-node"><span class="field-content"><a href="/faqs">+</a></span></div></div>');
 	});
     
     function boxes()
